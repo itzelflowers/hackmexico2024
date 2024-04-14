8a2a239d204f13fcc415ef91dae9baa6cefadb9a
Pablo Suárez Pérez <brekopablo@gmail.com>
2024-04-14 01:37:49 -0600

    Página principal

--- sections/login.py
+++ sections/login.py
@@ -1,6 +1,11 @@
 # Importar librerías necesarias.
 import streamlit as st
 from utils.firebase_utils import login_session
+# Conexión a base de datos.
+from utils.firebase import Firebase
+
+# Acceso a Firebase.
+db = Firebase().getdb()
 
 def app():
 
@@ -9,6 +14,12 @@ def app():
         st.session_state['ID'] = ''
     if 'user_type' not in st.session_state:
         st.session_state['user_type'] = ''
+    if 'name' not in st.session_state:
+        st.session_state['name'] = ''
+    if 'bss_type' not in st.session_state:
+        st.session_state['bss_type'] = ''
+    if 'last_name' not in st.session_state:
+        st.session_state['last_name'] = ''
     if 'signedout' not in st.session_state:
         st.session_state['signedout'] = False
     if 'signout' not in st.session_state:
@@ -20,21 +31,36 @@ def app():
         st.session_state['signedout'] = False
         st.session_state['signout'] = False
         st.session_state['ID'] = ''
-        st.session_state['first_name'] = ''
-        st.session_state['last_name'] = ''
+        if st.session_state['user_type'] == 'bussines':
+            st.session_state['name'] = ''
+            st.session_state['bss_type'] = ''
+        else:
+            st.session_state['name'] = ''
+            st.session_state['last_name'] = ''
         st.session_state['user_type'] = ''
     
+
     # Información de login.
     if not st.session_state['signedout']:
         st.sidebar.title("Inicio de Sesión") 
         st.sidebar.write("Inicia Sesión para ver más características")
         text_email = st.sidebar.text_input('Correo Electrónico', key='email')
         text_password = st.sidebar.text_input('Contraseña', type='password', key='password')
-
         # Enviar información.
         st.sidebar.button("Iniciar Sesión", on_click=login_session, args=(text_email, text_password))
     
+
+    # Sesión Iniciada.
     if st.session_state['signout']:
         st.sidebar.title("Bienvenido")
+        if st.session_state['user_type'] == 'bussines':
+            st.session_state['name'] = db.child(st.session_state.ID).child('name').get().val()
+            st.session_state['bss_type'] = db.child(st.session_state.ID).child('bss_type').get().val()
+            st.sidebar.subheader(f'{st.session_state["name"]}')
+            st.sidebar.markdown(f'**Giro de la empresa**: {st.session_state["bss_type"]}')
+        else:
+            st.session_state['name'] = db.child(st.session_state.ID).child('name').get().val()
+            st.session_state['last_name'] = db.child(st.session_state.ID).child('last_name').get().val()
+            st.sidebar.subheader(f'{st.session_state["name"]} {st.session_state["last_name"]}')
         st.sidebar.markdown(f'**Tipo de usuario**: {st.session_state["user_type"]}')
         st.sidebar.button("Cerrar Sesión", on_click=logout_session)
